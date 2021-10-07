c50b20c8b56a9becdd45f2be5acc446256a838a7
Javier Marin <jj@Javiers-MacBook-Pro.local>
2021-10-06 20:09:56 -0500

    added morgan and dotenv as well as minor changes

--- server.js
+++ server.js
@@ -1,76 +1,100 @@
 const express = require('express');
-const app = express();
 const port = 3000
 const mongoose = require('mongoose');
+const morgan = require("morgan");
+require("dotenv").config();
+const app = express();
 
-main().catch(err => console.log(err));
+let IntakeForm = require('./intakeform.js')
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/Group10');
-}
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+mongoose
+  .connect(process.env.MONGO_URL)   // read environment varibale from .env
+  .then(() => {
+    console.log("Database connection Success!");
   })
+  .catch((err) => {
+    console.error("Mongo Connection Error", err);
+  });
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+const PORT = process.env.PORT || 3000; //Declare the port number
 
-app.use(express.json());
+app.use(express.json()); //allows us to access request body as req.body
+app.use(morgan("dev"));  //enable incoming request logging in dev mode
 
-//import Intakeform schema
-let IntakeForm = require('./intakeform');
 
-//view all
-app.get('/view', (req, res, next) => {
+
+//POST
+app.post('/clients', (req, res, next) => {
+
+  IntakeForm.create(req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      // res.json(data)
+      res.send('health insurance is added to the database');
+    }
+  });
+});
+
+//GET
+app.get('/clients', (req, res, next) => {
   //very plain way to get all the data from the collection through the mongoose schema
   IntakeForm.find((error, data) => {
-      if (error) {
-        //here we are using a call to next() to send an error message back
-        return next(error)
-      } else {
-        res.json(data)
-      }
-    })
+    if (error) {
+      //here we are using a call to next() to send an error message back
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
 });
-//view by id
 
 
-//create
-app.post('/create', (req, res, next) => {
-  console.log(req.body);
-    IntakeForm.create(req.body, (error, data) => {
-        if (error) {
-          console.log(error)
-        } else {
-          // res.json(data)
-          res.send(req.body);
-        }
-  })});
-//delete
-  app.delete('/delete/:id', (req, res, next) => {
-    //mongoose will use _id of document
-    IntakeForm.findByIdAndRemove(req.params.id, (error, data) => {
-        if (error) {
-          console.log(error)
-        } else {
-          res.status(200).json({
-            msg: data
-          })
-        }
-       });
+//DELETE
+app.delete('/delete/:id', (req, res, next) => {
+  //mongoose will use _id of document
+  IntakeForm.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.status(200).json({
+        msg: data
+      })
+    }
+  });
 });
-//update (needs to be revisionist)
+
+
+
+//UPDATE
 app.put('/update/:id', (req, res, next) => {
-  IntakeForm.findByIdAndUpdate(req.params.id , {
-      $set: req.body
-    }, (error, data) => {
-      if (error) {
-        return next(error);
-      } else {
-        res.send('Student is edited via PUT');
-        console.log('Student successfully updated!', data)
-      }
-    })
+  IntakeForm.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.send('Student is edited via PUT');
+      console.log('Student successfully updated!', data)
+    }
+  })
 });
+
+
+app.listen(PORT, () => {
+  console.log("Server started listening on port : ", PORT);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode)
+    err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
+
+
+
+
+
