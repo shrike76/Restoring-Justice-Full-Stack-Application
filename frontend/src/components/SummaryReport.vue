110a61a280f296db28e8b5a2068ca2eae9f33680
Javier Marin <jjmarin777@gmail.com>
2021-11-11 13:22:17 -0600

    added comments

--- frontend/src/components/SummaryReport.vue
+++ frontend/src/components/SummaryReport.vue
@@ -28,7 +28,7 @@ export default {
   },
   created() {
     let apiURL = "http://localhost:3000/referrals";
-
+    //getting the referral data from backened
     axios
       .get(apiURL)
       .then((res) => (this.referralData = res.data))
@@ -36,33 +36,43 @@ export default {
   },
 
   methods: {
+    //takes the referral data and pushes only nessessary data to the caseManagers[]
+    //also calculates number of referrals each case manager is assigned to
+    // removes duplicates when there are multiple referrals assigned to a specific case manager
     setCaseManagerData() {
       this.referralData.forEach((currentReferralData) => {
+        //keeps track of number of referrals assiedn to a case manager
         let count = 0;
         for (let i = 0; i < this.referralData.length; i++) {
           if (
+            //converts to lowercase for easier comparison
             this.referralData[i].CaseManager.toLowerCase() ==
             currentReferralData.CaseManager.toLowerCase()
           ) {
+            //increment count when found referral that is assigned to a case manager
             count++;
           }
         }
-
+        //formatted data that will be pushed to caseManagers[]
         let payload = {
           CaseManager: currentReferralData.CaseManager,
           Count: count,
         };
 
+        //looks to see if there is already a case manager withing caseManagers[]
         let isThere = this.caseManagers.find(
           (caseManager) =>
             caseManager.CaseManager.toLowerCase() ==
             currentReferralData.CaseManager.toLowerCase()
         );
 
+        // if one already exists within caseManagers[] then it will be true else false
         isThere = isThere ? true : false;
 
+        //if false push to the caseManager[]
         if (!isThere) {
           this.caseManagers.push(payload);
+          //else console log message
         } else {
           console.log("here");
         }
